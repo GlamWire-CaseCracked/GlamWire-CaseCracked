a6e0de60ead5e0bff8d65054da52f2a05a1890ab
VacaSama <klc_moore@outlook.com>
2025-11-11 15:36:31 -0500

    referenceing **Pokedex** project-- (see repos),
    Using that to work on inserting the DB connection.

--- GlamwireDb.cs
+++ GlamwireDb.cs
@@ -11,4 +11,13 @@ public class GlamwireDb
     // string connection to the Glamwire database 
     string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GlamwireDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
+    /// <summary>
+    /// initializes a new instance of the GlamwireDb, 
+    /// retrieves data from the database and populates it into the application
+    /// </summary>
+    public GlamwireDb()
+    {
+
+    }
+
 }
