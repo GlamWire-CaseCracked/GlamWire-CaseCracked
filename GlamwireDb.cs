6b98cfb1e65cf46ad7e0fe058921dc5c11eda2b2
VacaSama <klc_moore@outlook.com>
2025-11-11 15:33:48 -0500

    GlamwireDB class made for the connection string to be added

---
+++ GlamwireDb.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlamWire_Case_Cracked;
+
+public class GlamwireDb
+{
+    // string connection to the Glamwire database 
+    string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GlamwireDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+}
